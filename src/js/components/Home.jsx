27c06632fef247e3da5943eedf542d15ef9a826a
Carlos Melchor Perez <carloos0665@gmail.com>
2025-03-17 23:45:03 +0000

    Add traffic Light

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,30 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
+import { useState } from "react";
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const [color, setcolor] = useState("");
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+	return (
+		<React.Fragment>
+			<div className="container">
+				<div className="semaforo">
+					<div
+						onClick={() => setcolor( color === "red" ? "" : "red" )}
+						className={`red light  ${(color === "red") ? "selected" : ""}`}
+					></div>
+
+					<div
+						onClick={() => setcolor( color === "yellow" ? "" : "yellow" )}
+						className={`yellow light ${(color === "yellow") ? "selected" : ""}`}
+					></div>
+
+					<div
+						onClick={() => setcolor( color === "green" ? "" : "green" )}
+						className={`green light ${(color === "green") ? "selected" : ""}`}
+					></div>
+				</div>
+			</div>
+		</React.Fragment >
 	);
 };
-
 export default Home;
