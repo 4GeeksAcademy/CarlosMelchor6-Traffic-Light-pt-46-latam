d8738be05224800470916e72d5725336f4f7f07c
Carlos Melchor Perez <carloos0665@gmail.com>
2025-03-18 04:13:04 +0000

    correcion de setcolor

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -2,24 +2,24 @@ import React from "react";
 import { useState } from "react";
 //create your first component
 const Home = () => {
-	const [color, setcolor] = useState("");
+	const [color, setColor] = useState("");
 
 	return (
 		<React.Fragment>
 			<div className="container">
-				<div className="semaforo">
+				<div className="trafficLight">
 					<div
-						onClick={() => setcolor( color === "red" ? "" : "red" )}
+						onClick={() => setColor( color === "red" ? "" : "red" )}
 						className={`red light  ${(color === "red") ? "selected" : ""}`}
 					></div>
 
 					<div
-						onClick={() => setcolor( color === "yellow" ? "" : "yellow" )}
+						onClick={() => setColor( color === "yellow" ? "" : "yellow" )}
 						className={`yellow light ${(color === "yellow") ? "selected" : ""}`}
 					></div>
 
 					<div
-						onClick={() => setcolor( color === "green" ? "" : "green" )}
+						onClick={() => setColor( color === "green" ? "" : "green" )}
 						className={`green light ${(color === "green") ? "selected" : ""}`}
 					></div>
 				</div>
